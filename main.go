ac776dad2ad6a923c985e13b728e4467508c45f5
Alexey Yundin <yundin@ekb.sima-land.ru>
2018-02-20 17:55:56 +0500

    start create go weather

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"log"
+	"html/template"
+)
+
+type WeatherTemp struct {
+	Temp     float64 `json:"temp"`
+	TempMin  float64 `json:"temp_min"`
+	TempMax  float64 `json:"temp_max"`
+	Humidity int32   `json:"humidity"`
+	Pressure int32   `json:"pressure"`
+}
+
+type WeatherWind struct {
+	Speed int32 `json:"speed"`
+	Deg   int32 `json:"deg"`
+} 
+
+type WeatherBase struct {
+	Base    string        `json:"base"`
+	Main    WeatherTemp   `json:"main"`
+	Name    string        `json:"name"`
+	Wind    WeatherWind   `json:"wind"`
+}
+
+func main() {
+	tmpl, err := template.New("template.html").ParseFiles("template.html")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		c := http.Client{}
+		resp, err := c.Get("http://api.openweathermap.org/data/2.5/weather?q=Yekaterinburg,ru&lang=ru&units=metric&appid=b0e8c750497d3d6add4e1b144715e5b2")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		weather := WeatherBase{}
+
+		err = json.Unmarshal(body, &weather)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = tmpl.Execute(w, weather)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.ListenAndServe(":8081", nil)
+}
